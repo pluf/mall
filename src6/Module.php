b1415b8a6137493fbfeaf2d0a8264c8972bf4373
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-14 18:43:49 +0330

    https://github.com/pluf/shop/issues/4

--- src6/Module.php
+++ src6/Module.php
@@ -3,5 +3,12 @@ namespace Pluf\Mall;
 
 class Module extends \Pluf\Module
 {
+
+    const moduleJsonPath = __DIR__ . '/module.json';
+
+    const relations = array();
+
+    public function init(Pluf $bootstrap): void
+    {}
 }
 
